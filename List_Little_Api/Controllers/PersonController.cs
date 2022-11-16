6391ba3e842e684b993268f734a50a8cf2e65962
RoadJDK <realroadjdk@gmail.com>
2022-11-16 14:09:59 +0100

    init

---
+++ List_Little_Api/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace List_Little_Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public bool GetAll()
+    {
+        return true;
+    }
+}
+
---
+++ List_Little_Api/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication()
+     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+     {
+         options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+         options.TokenValidationParameters =
+           new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+           {
+               ValidAudience = builder.Configuration["Auth0:Audience"],
+               ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}"
+           };
+     });
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
