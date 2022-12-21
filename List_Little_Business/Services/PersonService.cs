24fc91c2323f317bb672eb781d6f4a1cf0570733
RoadJDK <realroadjdk@gmail.com>
2022-12-21 10:58:57 +0100

    add services

---
+++ List_Little_Business/Services/PersonService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using List_Little_Business_Contracts.Services;
+using List_Little_Domain.Models;
+
+namespace List_Little_Business.Services
+{
+    public class PersonService : IPersonService
+    {
+        public IEnumerable<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ List_Little_Business_Contracts/Services/IPersonService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using List_Little_Domain.Models;
+
+namespace List_Little_Business_Contracts.Services
+{
+	public interface IPersonService
+	{
+		public IEnumerable<Person> GetAll();
+	}
+}
+
