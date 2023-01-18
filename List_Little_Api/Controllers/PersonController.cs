7b914f4d79d881220d7f9ee01216cabdd36a8f36
RoadJDK <realroadjdk@gmail.com>
2023-01-18 08:29:59 +0100

    add logging

--- List_Little_Api/Controllers/PersonController.cs
+++ List_Little_Api/Controllers/PersonController.cs
@@ -12,13 +12,11 @@ namespace List_Little_Api.Controllers;
 public class PersonController : ControllerBase
 {
     private readonly IPersonService Service;
-    private readonly IMapper Mapper;
     private readonly ILogger<PersonController> Logger;
 
-    public PersonController(IPersonService service, IMapper mapper, ILogger<PersonController> logger)
+    public PersonController(IPersonService service, ILogger<PersonController> logger)
     {
         Service = service;
-        Mapper = mapper;
         Logger = logger;
     }
 
@@ -27,27 +25,33 @@ public class PersonController : ControllerBase
     {
         try
         {
+            Logger.LogInformation("Authorisation Successful");
             if (User.HasClaim("permissions", "listlittle:read-write"))
             {
                 var personList = Service.GetAll();
+                Logger.LogInformation("Return Admin information");
                 return Ok(personList);
             }
             else if (User.HasClaim("permissions", "listlittle:read-write-one-eight"))
             {
                 var personList = Service.GetAll();
                 var sortedList = personList.OrderBy(p => p.Number).Take(8);
+                Logger.LogInformation("Return User1 information");
                 return Ok(sortedList);
             }
             else if (User.HasClaim("permissions", "listlittle:read-write-nine-sixteen"))
             {
                 var personList = Service.GetAll();
                 var sortedList = personList.OrderByDescending(p => p.Number).Take(8);
+                Logger.LogInformation("Return User2 information");
                 return Ok(sortedList);
             }
+            Logger.LogError("Something went wrong");
             return BadRequest();
         }
-        catch (Exception)
+        catch (Exception e)
         {
+            Logger.LogError("Something went wrong" + e);
             return BadRequest();
         }
     }
